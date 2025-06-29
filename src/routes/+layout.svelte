e880acac3bd6866e93498334d9ef8fbd3c8d50d2
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-06-30 04:56:18 +0700

    feat: init main

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '../app.css';
+	import { Metadata } from '$lib/components';
+
+	type Props = {
+		children: Snippet;
+	};
+
+	let { children }: Props = $props();
+</script>
+
+<Metadata />
+
+{@render children()}
